d4f786cb61f35140873ba55eabca0a6727a4d7ae
oleg <draconfantom@gmail.com>
2024-05-09 22:49:17 +0300

    my game

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lessons1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int lower, higer;
+            int triesCount = 5;
+            int userInput;
+            Random random = new Random();
+
+            number = random.Next(1,50);
+            lower = random.Next(number -1,number);
+            higer = random.Next(number +1,number +10);
+
+            Console.WriteLine($"Мы загадали число от 0 до 100, которое больше чем{lower},но меньше чем {higer}");
+            Console.WriteLine($"Что это за число? У нас {triesCount} попыток отгадать");
+
+            while( triesCount-- > 0 ) 
+            {
+                Console.Write("Ваш ответ: ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput == number)
+                {
+                    Console.WriteLine("вы правы это было число: " +number + ".");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Не верно попробуйте еще раз");
+                }
+
+                
+            }
+
+            if (triesCount <0)
+            {
+                Console.WriteLine("Вы проиграли! Это было число " + number + ".");
+            }
+            
+        }
+    }
+}
